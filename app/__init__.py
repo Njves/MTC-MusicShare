8eee43311e04c1356d26e118322eaf1755db88fa
Njves <mrpostik@gmail.com>
2023-11-25 11:24:53 +0700

    Добавить основной скелет сервера

---
+++ app/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_admin import Admin
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from sqlalchemy import MetaData
+
+from config import Config
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+migrate = Migrate()
+login_manager = LoginManager()
+admin_app = Admin(name='MusicShare', template_mode='bootstrap3')
+db = SQLAlchemy(metadata=MetaData(naming_convention=convention))
+login_manager.login_view = 'auth.login'
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    migrate.init_app(app, db,render_as_batch=True)
+    login_manager.init_app(app)
+    db.init_app(app)
+    admin_app.init_app(app)
+
+    from app.auth import bp as auth_bp
+
+    app.register_blueprint(auth_bp)
+
+    return app
+
+
+from app import models, admin
