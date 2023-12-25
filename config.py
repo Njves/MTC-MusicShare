f2f99096ccc877a944f7542b21bb68f38c8224ea
Njves <mrpostik@gmail.com>
2023-12-25 16:59:34 +0700

    Перешел на постгрес

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DB')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'some-secret-key'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MUSIC_DIR = pathlib.Path('app/static/music')
